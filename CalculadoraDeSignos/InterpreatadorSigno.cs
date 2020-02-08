3d512e28f9635efc34d9e0104da7fe5efd138ed0
Romulo <romulosousa27@hotmail.com>
2020-02-08 12:40:59 -0300

    Teste Interpretador de Signos

---
+++ CalculadoraDeSignos/InterpreatadorSigno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraDeSignos
+{
+    class InterpreatadorSigno
+    {
+        public Signo interpretar(int dia, int mes)
+        {
+            Signo signo = new null;
+
+            if ((dia >= 19 && mes == 2) || (dia >= 20 && mes == 3)) 
+            {
+                signo = new Signo();
+                signo.nome = "Peixes";
+            }
+
+            return signo;
+        }
+    }
+}

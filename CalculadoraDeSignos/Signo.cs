5661671ae109647060f726803d1a3e7b64160da6
Romulo <romulosousa27@hotmail.com>
2020-02-08 12:40:48 -0300

    Signo

---
+++ CalculadoraDeSignos/Signo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraDeSignos
+{
+    class Signo
+    {
+        public string nome;
+        public int diaInicio, mesInicio, diaFim, mesFim;
+        public string caracteristicas;
+    }
+}

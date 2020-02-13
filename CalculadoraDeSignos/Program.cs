8889adf1ccb2123ecc6692637e1c68b86e13cba4
romulo <romulosousa@fabricainfo.com>
2020-02-13 18:59:40 -0300

    aplicando a consulta e tratando os erros de exceçao

--- CalculadoraDeSignos/Program.cs
+++ CalculadoraDeSignos/Program.cs
@@ -6,10 +6,40 @@ namespace CalculadoraDeSignos
     {
         static void Main(string[] args)
         {
-            Signo signo = new Signo();
-            signo.nome = "Libra";
+            String dia;
+            String mes;
 
-            Console.WriteLine("Seu signo é: "+signo.nome);
+            Console.WriteLine("Dia de nascimento: ");
+            dia = Console.ReadLine();
+
+            Console.WriteLine("Mẽs do nascimento: ");
+            mes = Console.ReadLine();
+
+            int diaInt = 0;
+            int mesInt = 0;
+
+            try
+            {
+              diaInt = Convert.ToInt32(diaInt);
+              mesInt = Convert.ToInt32(mesInt);
+            }
+            catch (FormatException e)
+            {
+              Console.Write(e.Message);
+              Console.ReadLine();
+              Environment.Exit(1);
+            }
+
+            InterpreatadorSigno interpreatador = new InterpreatadorSigno();
+            Signo signo = interpreatador.interpretar(diaInt, mesInt);
+
+            if (signo != null)
+            {
+              Console.WriteLine("Seu Signo é: "+signo.nome+" "+signo.caracteristicas);
+            } else
+            {
+              Console.WriteLine("Não foi possivel interpretar seu signo!");
+            }
             Console.ReadLine();
         }
     }
